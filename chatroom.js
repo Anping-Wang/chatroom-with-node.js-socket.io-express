bee523491775e324c86ff03aebd286051f2d6804
Anping-Wang <anpingwang123@gmail.com>
2018-09-15 15:22:00 +0800

    完成基本功能

---
+++ chatroom.js
@@ -0,0 +1,32 @@
+'use strict';
+const path = require('path');
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'index.html'))
+});
+let users = {};
+let count = 0;
+io.on('connection',(socket) => {
+    console.log('connecting...');
+    socket.on('login',(data) => {
+        //因为每个用户的socket对象是独立的，利用这个特性，把每个用户的用户名信息存到自己的socket对象上
+        socket.username = data;
+        count++;
+        users[data]=data;
+        // console.log(username);
+        socket.emit('welcome',data);
+        socket.broadcast.emit('welcome',data);
+        socket.emit('list',{count:count,users:users});
+        socket.broadcast.emit('list',{count:count,users:users});
+    });
+    socket.on('publish',function (data) {
+        socket.emit('publish',{username:socket.username,msg:data});
+        socket.broadcast.emit('publish',{username:socket.username,msg:data});
+    })
+});
+
+server.listen(4000,() => {
+    console.log('server is listening in port 4000')
+});
